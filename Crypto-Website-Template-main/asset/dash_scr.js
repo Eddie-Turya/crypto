04ae09efa4d7707b35525a990420bb378b81d953
Edwin Turya <164741494+Eddie-Turya@users.noreply.github.com>
2024-10-06 14:02:33 -0700

    Delete Crypto-Website-Template-main directory

--- Crypto-Website-Template-main/asset/dash_scr.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleBtn = document.querySelector('.toggle-btn');
-    const sidebar = document.querySelector('.sidebar');
-
-    toggleBtn.addEventListener('click', () => {
-        sidebar.classList.toggle('collapsed');
-    });
-});
